74ff91f1226afa8197f2a7015a5ff7b4e44e3167
zakaz <adam.staron@vp.pl>
2023-08-22 22:03:43 +0200

    another-main-update

--- src/main.js
+++ src/main.js
@@ -9,22 +9,29 @@ function Main(){
     
     return (
         <div className="app-main">
-          <div className="app-main-note-edit">
-            <input
-              type="text"
-              id="title"
-              placeholder="Note Title"
-              value={activeNote.title}
-              onChange={(e) => onEditField("title", e.target.value)}
-            />
-          </div>
-          <div className="app-main-note-preview">
-            <h1 className="preview-title">{activeNote.title}</h1>
-            <ReactMarkdown className="markdown-preview">
-              {activeNote.body}
-            </ReactMarkdown>
-          </div>
+        <div className="app-main-note-edit">
+          <input
+            type="text"
+            id="title"
+            placeholder="Note Title"
+            value={activeNote.title}
+            onChange={(e) => onEditField("title", e.target.value)}
+            autoFocus
+          />
+          <textarea
+            id="body"
+            placeholder="Write your note here..."
+            value={activeNote.body}
+            onChange={(e) => onEditField("body", e.target.value)}
+          />
         </div>
+        <div className="app-main-note-preview">
+          <h1 className="preview-title">{activeNote.title}</h1>
+          <ReactMarkdown className="markdown-preview">
+            {activeNote.body}
+          </ReactMarkdown>
+        </div>
+      </div>
       );
 }
 export default Main
