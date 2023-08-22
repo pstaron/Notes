c4aa01138ca7f50c8c57c8033e1a004fa86e13c3
zakaz <adam.staron@vp.pl>
2023-08-22 22:03:15 +0200

    main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 function Main(){
-
+    const onEditField = (field, value) => {
+        onUpdateNote({
+          ...activeNote,
+          [field]: value,
+          lastModified: Date.now(),
+        });
+      };
+    
+    return (
+        <div className="app-main">
+          <div className="app-main-note-edit">
+            <input
+              type="text"
+              id="title"
+              placeholder="Note Title"
+              value={activeNote.title}
+              onChange={(e) => onEditField("title", e.target.value)}
+            />
+          </div>
+          <div className="app-main-note-preview">
+            <h1 className="preview-title">{activeNote.title}</h1>
+            <ReactMarkdown className="markdown-preview">
+              {activeNote.body}
+            </ReactMarkdown>
+          </div>
+        </div>
+      );
 }
 export default Main
