37da066af927fd41b0c0ad5abf85b02574954e97
zakaz <adam.staron@vp.pl>
2023-08-23 21:28:54 +0200

    App changesv1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import uuid from "react-uuid";
+import "./App.css";
+import Main from "./main/Main";
+import Sidebar from "./sidebar/Sidebar";
+
+function App() {
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+  const [activeNote, setActiveNote] = useState(false);
+
+  
+  const onDeleteNote = (noteId) => {
+    setNotes(notes.filter(({ id }) => id !== noteId));
+  };
+
+
+  return (
+    <div className="App">
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+    </div>
+  );
+}
+
+export default App;
